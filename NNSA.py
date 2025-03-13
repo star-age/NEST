bea689d45bbc9cf84d70fceb8a643a3c6be0d5ce
titanbin <agesstellar@gmail.com>
2025-03-13 10:53:15 +0100

    fixed mode, added HST and tqdm

--- NNSA.py
+++ NNSA.py
@@ -1,9 +1,15 @@
 import numpy as np
-import pickle,json
+import pickle,json,os
+try:
+    from tqdm import tqdm
+    has_tqdm = True
+except ImportError:
+    has_tqdm = False
 
 def available_models():
-    model_list = ['BaSTI','PARSEC','MIST','SYCLIST','Dartmouth','YaPSI']
+    model_list = ['BaSTI','BaSTI_HST','PARSEC','MIST','SYCLIST','Dartmouth','YaPSI']
     model_sources = [
+        'http://basti-iac.oa-abruzzo.inaf.it/',
         'http://basti-iac.oa-abruzzo.inaf.it/',
         'https://stev.oapd.inaf.it/PARSEC/',
         'https://waps.cfa.harvard.edu/MIST/',
@@ -15,12 +21,15 @@ def available_models():
         print(model + 'Model (' + source + ')')
 
 class AgeModel:
-    def __init__(self,model_name,use_sklearn=True):
+    def __init__(self,model_name,use_sklearn=True,use_tqdm=True):
         self.model_name = model_name
         self.use_sklearn = use_sklearn
-        self.space_BP_RP = np.linspace(-1,3,101)
-        self.space_MG = np.linspace(-5,10,101)
-        self.space_MH = np.linspace(-3,0.5,11)
+        self.use_tqdm = use_tqdm
+        if not has_tqdm and self.use_tqdm:
+            self.use_tqdm = False
+        self.space_col = np.linspace(-1,3,101)
+        self.space_mag = np.linspace(-5,10,101)
+        self.space_met = np.linspace(-3,0.5,11)
         self.domain = np.load('domain.npy',allow_pickle=True).item()[model_name]
         self.neural_networks = {}
         self.scalers = {}
@@ -32,48 +41,52 @@ class AgeModel:
 
     def load_neural_network(self, model_name):
         if self.use_sklearn:
-            self.neural_networks['full'] = pickle.load(open('models/NN_{}.sav'.format(model_name), 'rb'))
-            self.scalers['full'] = pickle.load(open('models/scaler_{}.sav'.format(model_name), 'rb'))
-            self.neural_networks['reduced'] = pickle.load(open('models/NN_{}_BPRP.sav'.format(model_name), 'rb'))
-            self.scalers['reduced'] = pickle.load(open('models/scaler_{}_BPRP.sav'.format(model_name), 'rb'))
+            if os.path.exists('models/NN_{}.sav'.format(model_name)):
+                self.neural_networks['full'] = pickle.load(open('models/NN_{}.sav'.format(model_name), 'rb'))
+                self.scalers['full'] = pickle.load(open('models/scaler_{}.sav'.format(model_name), 'rb'))
+            if os.path.exists('models/NN_{}_BPRP.sav'.format(model_name)):
+                self.neural_networks['reduced'] = pickle.load(open('models/NN_{}_BPRP.sav'.format(model_name), 'rb'))
+                self.scalers['reduced'] = pickle.load(open('models/scaler_{}_BPRP.sav'.format(model_name), 'rb'))
         else:
-            json_nn = json.load(open('models/NN_{}.json'.format(model_name), 'r'))
-            self.neural_networks['full'] = {
-                'weights':json_nn['weights'],
-                'biases':json_nn['biases']
-            }
-            self.scalers['full'] = {
-                'means':json_nn['means'],
-                'stds':json_nn['stds']
-            }
-            json_nn = json.load(open('models/NN_{}_BPRP.json'.format(model_name), 'r'))
-            self.neural_networks['reduced'] = {
-                'weights':json_nn['weights'],
-                'biases':json_nn['biases']
-            }
-            self.scalers['reduced'] = {
-                'means':json_nn['means'],
-                'stds':json_nn['stds']
-            }
+            if os.path.exists('models/NN_{}.json'.format(model_name)):
+                json_nn = json.load(open('models/NN_{}.json'.format(model_name), 'r'))
+                self.neural_networks['full'] = {
+                    'weights':json_nn['weights'],
+                    'biases':json_nn['biases']
+                }
+                self.scalers['full'] = {
+                    'means':json_nn['means'],
+                    'stds':json_nn['stds']
+                }
+            if os.path.exists('models/NN_{}_BPRP.json'.format(model_name)):
+                json_nn = json.load(open('models/NN_{}_BPRP.json'.format(model_name), 'r'))
+                self.neural_networks['reduced'] = {
+                    'weights':json_nn['weights'],
+                    'biases':json_nn['biases']
+                }
+                self.scalers['reduced'] = {
+                    'means':json_nn['means'],
+                    'stds':json_nn['stds']
+                }
 
     def ages_prediction(self,
-                        MH,MG,BPRP,
-                        eMH=None,eMG=None,eBPRP=None,
+                        met,mag,col,
+                        emet=None,emag=None,ecol=None,
                         GBP=None,GRP=None,
                         eGBP=None,eGRP=None,
                         n=1):
-        if type(MH) is not list:
-            MH = [MH]
-        if type(MG) is not list:
-            MG = [MG]
-        if type(BPRP) is not list:
-            BPRP = [BPRP]
-        if type(eMH) is not list and eMH is not None:
-            eMH = [eMH]
-        if type(eMG) is not list and eMG is not None:
-            eMG = [eMG]
-        if type(eBPRP) is not list and eBPRP is not None:
-            eBPRP = [eBPRP]
+        if type(met) is not list:
+            met = [met]
+        if type(mag) is not list:
+            mag = [mag]
+        if type(col) is not list:
+            col = [col]
+        if type(emet) is not list and emet is not None:
+            emet = [emet]
+        if type(emag) is not list and emag is not None:
+            emag = [emag]
+        if type(ecol) is not list and ecol is not None:
+            ecol = [ecol]
         if type(GBP) is not list and GBP is not None:
             GBP = [GBP]
         if type(GRP) is not list and GRP is not None:
@@ -82,7 +95,7 @@ class AgeModel:
             eGBP = [eGBP]
         if type(eGRP) is not list and eGRP is not None:
             eGRP = [eGRP]
-        inputs = [input for input in [MH,MG,BPRP,eMH,eMG,eBPRP,GBP,GRP,eGBP,eGRP] if input is not None]
+        inputs = [input for input in [met,mag,col,emet,emag,ecol,GBP,GRP,eGBP,eGRP] if input is not None]
         if len(set(map(len,inputs))) != 1:
             raise ValueError('All input arrays must have the same length')
 
@@ -90,34 +103,43 @@ class AgeModel:
         has_errors = False
         if GBP is not None and GRP is not None:
             is_reduced = False
-            if eMH is not None and eMG is not None and eBPRP is not None and eGBP is not None and eGRP is not None:
+            if emet is not None and emag is not None and ecol is not None and eGBP is not None and eGRP is not None:
                 has_errors = True
         else:
-            if eMH is not None and eMG is not None and eBPRP is not None:
+            if emet is not None and emag is not None and ecol is not None:
                 has_errors = True
         
         if n > 1 and not has_errors:
             raise ValueError('For more than one sample, errors must be provided')
         
         if is_reduced:
-            X = np.array([MH, MG, BPRP])
-            X_errors = np.array([eMH, eMG, eBPRP])
+            X = np.array([met, mag, col])
+            X_errors = np.array([emet, emag, ecol])
         else:
-            X = np.array([MH, MG, GBP, GRP, BPRP])
-            X_errors = np.array([eMH, eMG, eGBP, eGRP, eBPRP])
+            X = np.array([met, mag, GBP, GRP, col])
+            X_errors = np.array([emet, emag, eGBP, eGRP, ecol])
 
         X = X.T
         X_errors = X_errors.T
 
         if X.shape[1] == 3:
+            if self.neural_networks.get('reduced') is None:
+                raise ValueError('Reduced neural network not available for this model')
             scaler = self.scalers['reduced']
             neural_network = self.neural_networks['reduced']
         else:
+            if self.neural_networks.get('full') is None:
+                raise ValueError('Full neural network not available for this model')
             scaler = self.scalers['full']
             neural_network = self.neural_networks['full']
 
         self.age_predictions = np.zeros((X.shape[0],n))
-        for i in range(X.shape[0]):
+
+        if self.use_tqdm and (n > 1 or X.shape[0] > 1):
+            loop = tqdm(range(X.shape[0]))
+        else:
+            loop = range(X.shape[0])
+        for i in loop:
             if n > 1:
                 X_i = np.random.normal(X[i],X_errors[i],(n,X.shape[1]))
             else:
@@ -126,38 +148,38 @@ class AgeModel:
         
         return self.age_predictions
     
-    def check_domain(self,MH,MG,BPRP,eMH=None,eMG=None,eBPRP=None):
-        if type(MH) is not list:
-            MH = [MH]
-        if type(MG) is not list:
-            MG = [MG]
-        if type(BPRP) is not list:
-            BPRP = [BPRP]
+    def check_domain(self,met,mag,col,emet=None,emag=None,ecol=None):
+        if type(met) is not list:
+            met = [met]
+        if type(mag) is not list:
+            mag = [mag]
+        if type(col) is not list:
+            col = [col]
         
-        has_errors = eMH != None and eMG != None and eBPRP != None
+        has_errors = emet != None and emag != None and ecol != None
 
         if has_errors:
-            if type(eMH) is not list:
-                eMH = [eMH]
-            if type(eMG) is not list:
-                eMG = [eMG]
-            if type(eBPRP) is not list:
-                eBPRP = [eBPRP]
+            if type(emet) is not list:
+                emet = [emet]
+            if type(emag) is not list:
+                emag = [emag]
+            if type(ecol) is not list:
+                ecol = [ecol]
         
-        in_domain = np.zeros(len(MH),dtype=bool)
-        for i in range(len(MH)):
+        in_domain = np.zeros(len(met),dtype=bool)
+        for i in range(len(met)):
             if has_errors:
-                errors = [eBPRP[i],eMG[i],eMH[i]]
+                errors = [ecol[i],emag[i],emet[i]]
             else:
                 errors = [0,0,0]
-            min_i_BPRP = np.maximum(np.digitize(BPRP[i] - errors[0],self.space_BP_RP) - 1,0)
-            max_i_BPRP = np.minimum(np.digitize(BPRP[i] + errors[0],self.space_BP_RP) - 1,99)
-            min_i_MG = np.maximum(np.digitize(MG[i] - errors[1],self.space_MG) - 1,0)
-            max_i_MG = np.minimum(np.digitize(MG[i] + errors[1],self.space_MG) - 1,99)
-            min_i_MH = np.maximum(np.digitize(MH[i] - errors[2],self.space_MH) - 1,0)
-            max_i_MH = np.minimum(np.digitize(MH[i] + errors[2],self.space_MH) - 1,9)
+            min_i_col = np.maximum(np.digitize(col[i] - errors[0],self.space_col) - 1,0)
+            max_i_col = np.minimum(np.digitize(col[i] + errors[0],self.space_col) - 1,99)
+            min_i_mag = np.maximum(np.digitize(mag[i] - errors[1],self.space_mag) - 1,0)
+            max_i_mag = np.minimum(np.digitize(mag[i] + errors[1],self.space_mag) - 1,99)
+            min_i_met = np.maximum(np.digitize(met[i] - errors[2],self.space_met) - 1,0)
+            max_i_met = np.minimum(np.digitize(met[i] + errors[2],self.space_met) - 1,9)
 
-            in_domain[i] = bool(np.any(self.domain[min_i_BPRP:max_i_BPRP+1,min_i_MG:max_i_MG+1,min_i_MH:max_i_MH+1]) == 1)
+            in_domain[i] = bool(np.any(self.domain[min_i_col:max_i_col+1,min_i_mag:max_i_mag+1,min_i_met:max_i_met+1]) == 1)
         return in_domain
     
     def propagate(self,X,neural_network,scaler):
@@ -209,8 +231,11 @@ class AgeModel:
             raise ValueError('No age predictions have been made yet')
         #TODO: choose number of bins appropriately
         modes = []
+        min_age = max(0,self.age_predictions.min())
+        max_age = max(14,self.age_predictions.max())
+
         for i in range(len(self.age_predictions)):
-            hist, bins = np.histogram(self.age_predictions[i],bins=140,range=(0,14))
+            hist, bins = np.histogram(self.age_predictions[i],bins=100,range=(min_age,max_age))
             modes.append(bins[np.argmax(hist)] + (bins[1]-bins[0])/2)
         return np.array(modes)
     
@@ -222,6 +247,10 @@ class AgeModel:
 class BaSTIModel(AgeModel):
     def __init__(self,use_sklearn=True):
         super().__init__('BaSTI',use_sklearn)
+
+class BaSTI_HSTModel(AgeModel):
+    def __init__(self,use_sklearn=True):
+        super().__init__('BaSTI_HST',use_sklearn)
 
 class PARSECModel(AgeModel):
     def __init__(self,use_sklearn=True):
